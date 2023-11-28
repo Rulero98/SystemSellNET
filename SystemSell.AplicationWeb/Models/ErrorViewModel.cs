e59293fb865ea94daa53a0cd0605bd0d9a4bb91b
Raúl <rortegaa1601@alumno.ipn.mx>
2023-11-27 18:29:55 -0600

    first commit

---
+++ SystemSell.AplicationWeb/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace SystemSell.AplicationWeb.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
---
+++ SystemSell.IOC/Dependency.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using SystemSell.DALL.DBContext;
+using Microsoft.EntityFrameworkCore;
+//using SystemSell.DALL.Implementation;
+//using SystemSell.DALL.Interfaces;
+//using SystemSell.BLL.Interfaces;
+//using SystemSell.BLL.Interfaces;
+
+namespace SystemSell.IOC
+{
+    public static class Dependency
+    {
+        public static void InjectDependency(this IServiceCollection services, IConfiguration configuration) 
+        {
+            services.AddDbContext<DBVENTAContext>(options =>
+            {
+                options.UseSqlServer(configuration.GetConnectionString("ChainSQL"));
+            });
+        }
+    }
+}
